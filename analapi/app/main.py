fbfe45baa739bb74543704be57efd6333235dd09
Alexey Ivanov <alexeyivanov@yandex.ru>
2021-08-20 22:17:22 +0000

    Add docker

--- analapi/app/main.py
+++ analapi/app/main.py
@@ -1,10 +1,19 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import hello_router, certificates_router, transactions_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(hello_router)
 app.include_router(certificates_router)
