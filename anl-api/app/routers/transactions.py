6bfbfd8b66c7b9200a26016e69fad7459f7cd629
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2021-08-21 02:33:29 +0200

    refactor: refactor routers

--- anl-api/app/routers/transactions.py
+++ anl-api/app/routers/transactions.py
@@ -2,34 +2,27 @@ from fastapi import APIRouter
 
 from pydantic import BaseModel
 
+
 class Transaction(BaseModel):
     id: int = 0
-    status: str = "Complete/Incomplete" # 1 - транзакция оформлена, завершена 8 - транзакция оформлена, производство незавершено
-    incomplete: bool = True # 0 - обычное 1- незавершенное производство
-    common_hs: str = "Common HS" # ХС Integer business_entity
-    common_enterprise: str = "Common enterprise" # Предприятие Integer enterprise
-    insertDate: str = "InsertDate" # дата создания транзакции Timestamp
-    formedDate: str = "FormedDate" # дата оформления транзакции Timestamp
+    # 1 - транзакция оформлена, завершена 8 - транзакция оформлена, производство незавершено
+    status: str = "Complete/Incomplete"
+    incomplete: bool = True  # 0 - обычное 1- незавершенное производство
+    common_hs: str = "Common HS"  # ХС Integer business_entity
+    common_enterprise: str = "Common enterprise"  # Предприятие Integer enterprise
+    insertDate: str = "InsertDate"  # дата создания транзакции Timestamp
+    formedDate: str = "FormedDate"  # дата оформления транзакции Timestamp
 
 
-TRANSACTIONS = [
-    Transaction(**{
-        "id": 1
-    }),
-    Transaction(**{
-        "id": 2
-    }),
-    Transaction(**{
-        "id": 3
-    })
-]
+TRANSACTIONS = [Transaction(**{"id": 1}), Transaction(**{"id": 2}), Transaction(**{"id": 3})]
 
 
 router = APIRouter()
 
+
 @router.get("/transactions")
 async def get_transactions():
-    return TRANSACTIONS;
+    return TRANSACTIONS
 
 
 @router.get("/transactions/{transaction_id}")
