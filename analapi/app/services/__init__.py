96e56ce0d8b7d7bfcedcb9839f9332ec5e99015a
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2021-08-21 02:05:06 +0200

    feat: add postgres connection service

---
+++ analapi/app/services/__init__.py
@@ -0,0 +1,3 @@
+from .postgres import session_scope, Session
+
+__all__ = "session_scope", "Session"
---
+++ analapi/app/services/postgres.py
@@ -0,0 +1,26 @@
+from contextlib import contextmanager
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from app.settings import settings
+
+
+engine = create_engine(settings.POSTGRES_DSN, echo=settings.ENABLE_DEVELOP_MOD, future=True)
+session_maker = sessionmaker(
+    bind=engine,
+    autoflush=True,
+    # class_=AsyncSession,
+    # autocommit=True,
+)
+
+
+@contextmanager
+def session_scope():
+    session: Session = session_maker()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
