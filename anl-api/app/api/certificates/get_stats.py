7d812be018c3380f91ecbd80e5e3e68e8a5c9fcb
Alexey Ivanov <alexeyivanov@yandex.ru>
2021-08-21 11:07:22 +0000

    Fix mistakes counting

--- anl-api/app/api/certificates/get_stats.py
+++ anl-api/app/api/certificates/get_stats.py
@@ -1,21 +1,29 @@
-from app.models.certificate_score import CertificateScore
+from sqlalchemy.sql.expression import and_
 from typing import Optional
 
-from sqlalchemy.sql.expression import outerjoin
+from sqlalchemy import and_
 import app.models as m
 from app.services.postgres import session_scope
 from .schemas import Stats
 from sqlalchemy import func
+from app.settings import settings
 
 
 def get_stats() -> Optional[Stats]:
     with session_scope() as session:
-        result = (session.query(
-            func.count(m.Certificate.doctor_id.distinct()).label("doctors_count"),
-            func.count(m.Certificate.id.distinct()).label("certificates_count"),
-            func.count(m.CertificateScore.certificate_id.distinct()).label("mistakes_count"),
-        )
-            .outerjoin(CertificateScore)
+        result = (
+            session.query(
+                func.count(m.Certificate.doctor_id.distinct()).label("doctors_count"),
+                func.count(m.Certificate.id.distinct()).label("certificates_count"),
+                func.count(m.CertificateScore.certificate_id.distinct()).label("mistakes_count"),
+            )
+            .outerjoin(
+                m.CertificateScore,
+                and_(
+                    m.Certificate.id == m.CertificateScore.certificate_id,
+                    m.CertificateScore.score > settings.SCORE_ERROR_THRESHOLD,
+                ),
+            )
             .first()
         )
 
